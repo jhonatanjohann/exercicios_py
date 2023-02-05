e7d7cdf0d7b378b53d8d92886312a12ce8345abc
jhonatanjohann <jhon8gf@gmail.com>
2023-02-04 21:42:57 -0300

    exercicio 004

---
+++ exercicios/exercicio_004.py
@@ -0,0 +1,6 @@
+numero = int(input("digite a nota do primeiro bimestre: "))
+numero2 = int(input("digite a nota do segundo bimestre: "))
+numero3 = int(input("digite a nota do terceiro bimestre: "))
+numero4 = int(input("digite a nota do quarto bimestre: "))
+
+print("a media dos quatro bimestre foi: {0}".format((numero + numero2 + numero3 + numero4) / 4))
