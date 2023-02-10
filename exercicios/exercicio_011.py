0840cf60738f0884aecebf1a06616f63e339c9c6
jhonatanjohann <jhon8gf@gmail.com>
2023-02-09 23:07:25 -0300

    exercicio 11

---
+++ exercicios/exercicio_011.py
@@ -0,0 +1,10 @@
+inteiro = float(input("um numero intiro: "))
+inteiro2 = float(input("outro numero intiro: "))
+real = float(input("um numero real: "))
+
+print("A: {0}".format(inteiro * 2))
+print("   {0}".format(inteiro2 / 2))
+
+print("B: {0}".format(((inteiro * 3) + real)))
+
+print("C: {0}".format((real * real * real)))
